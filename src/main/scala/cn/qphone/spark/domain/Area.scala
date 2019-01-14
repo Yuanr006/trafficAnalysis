ab8af3425c8075c791d89d4e1804d20fae260ef4
zhuGuangyuan <1159814737@qq.com>
2019-01-14 22:55:00 +0800

    基础

--- src/main/scala/cn/qphone/spark/domain/Area.scala
+++ src/main/scala/cn/qphone/spark/domain/Area.scala
@@ -1,10 +1,10 @@
-package cn.qphone.spark.dao.impl
+package cn.qphone.spark.domain
 
 /**
   * @Auther: zhuguangyuan 1159814737@qq.com
-  * @Date: 2019/1/14 22:22
+  * @Date: 2019/1/14 22:23
   * @Description: ${Description}
   */
-class AreaDaoImpl {
+class Area {
 
 }
