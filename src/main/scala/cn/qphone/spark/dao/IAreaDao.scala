862af9c3df2dcb06778a3ba439515f81c10e5133
zhuGuangyuan <1159814737@qq.com>
2019-01-14 22:55:00 +0800

    基础

---
+++ src/main/scala/cn/qphone/spark/dao/IAreaDao.scala
@@ -0,0 +1,10 @@
+package cn.qphone.spark.dao
+
+/**
+  * @Auther: zhuguangyuan 1159814737@qq.com
+  * @Date: 2019/1/14 22:23
+  * @Description: ${Description}
+  */
+class IAreaDao {
+
+}
---
+++ src/main/scala/cn/qphone/spark/rtmroad/RoadRealTimeAnalyze.scala
@@ -0,0 +1,10 @@
+package cn.qphone.spark.rtmroad
+
+/**
+  * @Auther: zhuguangyuan 1159814737@qq.com
+  * @Date: 2019/1/14 22:23
+  * @Description: ${Description}
+  */
+object RoadRealTimeAnalyze {
+
+}
