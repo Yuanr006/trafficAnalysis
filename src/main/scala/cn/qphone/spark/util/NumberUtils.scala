64e9e30b45ff19667a524e19a9b3b9f06d47c75c
Yuanr006 <32871049+Yuanr006@users.noreply.github.com>
2019-01-15 09:51:26 +0800

    数字格式化器--王佳慧

---
+++ src/main/scala/cn/qphone/spark/util/NumberUtils.scala
@@ -0,0 +1,19 @@
+package org.qphone.scala.Utils
+
+object NumberUtils {
+
+  import java.math.BigDecimal
+
+  /**
+    * 格式化小数
+    *
+    * @param num   字符串?
+    * @param scale 四舍五入的位数
+    * @return 格式化小数
+    */
+  def formatDouble(num: Double, scale: Int): Double = {
+    val bd = new BigDecimal(num)
+    bd.setScale(scale, BigDecimal.ROUND_HALF_UP).doubleValue
+  }
+
+}
