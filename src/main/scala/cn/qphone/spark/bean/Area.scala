ceef4d8224abd7d987fda72962f54a55269ca133
zhuGuangyuan <1159814737@qq.com>
2019-01-15 10:52:25 +0800

    描述：ScalaBean Area.scala 的编写； 位置：cm.qphone.spark.bean.Area.scala。

--- src/main/scala/cn/qphone/spark/bean/Area.scala
+++ src/main/scala/cn/qphone/spark/bean/Area.scala
@@ -1,10 +1,44 @@
 package cn.qphone.spark.bean
 
-import scala.beans.BeanProperty
+/**
+  * @Type: ScalaBean
+  * @Description: 区域类
+  */
 
-abstract class Area {
-  @BeanProperty
-  var areaId: String = null
-  @BeanProperty
-  var areaName: String = null
-}
+ class Area() {
+
+  /**
+    *  全参构造器
+    * @param id 区域编号
+    * @param name 区域名称
+    */
+   def this(id:String,name:String){
+     this
+     areaId=id
+     areaName=name
+   }
+
+   /**
+     * 区域编号
+     */
+   private[this] var _areaId: String = ""
+
+   def areaId: String = _areaId
+
+   def areaId_=(value: String): Unit = {
+     _areaId = value
+   }
+
+   /**
+     * 区域名称
+     */
+   private[this] var _areaName: String = ""
+
+   def areaName: String = _areaName
+
+   def areaName_=(value: String): Unit = {
+     _areaName = value
+   }
+    //toString
+   override def toString = s"Area($_areaId, $_areaName, $areaId, $areaName)"
+ }
