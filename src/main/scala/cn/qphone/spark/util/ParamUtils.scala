48c0da916586fbe2748fd0bc3e3f7839e98059f8
xjw <897379351@qq.com>
2019-01-15 12:21:02 +0800

    dao更新

--- src/main/scala/cn/qphone/spark/util/ParamUtils.scala
+++ src/main/scala/cn/qphone/spark/util/ParamUtils.scala
@@ -1,4 +1,6 @@
-import com.alibaba.fastjson.{JSON, JSONObject}
+import cn.qphone.spark.conf.ConfigurationManager
+import cn.qphone.spark.constant.Constants
+//import com.alibaba.fastjson.{JSON, JSONObject}
 
 
 /**
@@ -6,33 +8,33 @@ import com.alibaba.fastjson.{JSON, JSONObject}
   * @author Administrator
   *
   */
-object ParamUtils {
-  /**
-    * 从命令行参数中提取任务id
-    * @param args 命令行参数
-    * @param taskType 参数类型(任务id对应的值是Long类型才可以)，对应my.properties中的key
-    * @return 任务id
-    */
-  def getTaskIdFromArgs(args:Array[String],taskType:String): Long ={
-    val local = ConfigurationManager.getBoolean(Constants.SPARK_LOCAL)
-    if(local) {
-      ConfigurationManager.getLong(taskType)
-    } else {
-        if(args != null && args.length > 0) {
-           String.valueOf(args(0)).toLong
-        }else 0
-    }
-  }
-  /**
-    * 从JSON对象中提取参数
-    * @param jsonObject JSON对象
-    * @return 参数
-    * {"name":"zhangsan","age":"18"}
-    */
-  def getParam(jsonObject:JSONObject,field:String): String  ={
-    val jsonArray = jsonObject.getJSONArray(field)
-    if(jsonArray != null && jsonArray.size() > 0) {
-       jsonArray.getString(0)
-    }else null
-  }
-}
+//object ParamUtils extends Constants{
+//  /**
+//    * 从命令行参数中提取任务id
+//    * @param args 命令行参数
+//    * @param taskType 参数类型(任务id对应的值是Long类型才可以)，对应my.properties中的key
+//    * @return 任务id
+//    */
+//  def getTaskIdFromArgs(args:Array[String],taskType:String): Long ={
+//    val local = ConfigurationManager.getBoolean(SPARK_LOCAL)
+//    if(local) {
+//      ConfigurationManager.getLong(taskType)
+//    } else {
+//        if(args != null && args.length > 0) {
+//           String.valueOf(args(0)).toLong
+//        }else 0
+//    }
+//  }
+//  /**
+//    * 从JSON对象中提取参数
+//    * @param jsonObject JSON对象
+//    * @return 参数
+//    * {"name":"zhangsan","age":"18"}
+//    */
+//  def getParam(jsonObject:JSONObject,field:String): String  ={
+//    val jsonArray = jsonObject.getJSONArray(field)
+//    if(jsonArray != null && jsonArray.size() > 0) {
+//       jsonArray.getString(0)
+//    }else null
+//  }
+//}
