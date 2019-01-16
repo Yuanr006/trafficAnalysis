e972845e54762a33d89d5d955942d37a7a2d9f4d
as897379351 <44804663+as897379351@users.noreply.github.com>
2019-01-16 19:45:43 +0800

    卡扣流量模块新建

---
+++ src/main/scala/cn/qphone/spark/skynet/MonitorFlowAnalyze.scala
@@ -0,0 +1,125 @@
+package cn.qphone.spark.skynet
+
+import cn.qphone.spark.bean.{MonitorState, TopNMonitor2CarCount}
+import cn.qphone.spark.constant.Constants
+import cn.qphone.spark.dao.factory.DAOFactory
+import cn.qphone.spark.mockData.MockData
+import cn.qphone.spark.util.ParamUtils
+import com.alibaba.fastjson.{JSON, JSONObject}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Row, SQLContext}
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
+
+
+object MonitorFlowAnalyze{
+  /**
+    * 基于本地测试生成模拟测试数据，如果在集群中运行的话，直接操作Hive中的表就可以
+    * 本地模拟数据注册成一张临时表
+    * monitor_flow_action	数据表：监控车流量所有数据
+    * monitor_camera_info	标准表：卡扣对应摄像头标准表
+    */
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName(Constants.SPARK_APP_NAME_SESSION).setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
+    MockData.mock(sc, sqlContext)
+    val rdd1 = MockData.rdd1.cache()
+    val rdd2 = MockData.rdd2.cache()
+    /**
+      * 从配置文件my.properties中拿到spark.local.taskId.monitorFlow的taskId
+      */
+    val taskId = 1
+    /**
+      * 获取ITaskDAO的对象，通过taskId查询出来的数据封装到Task（自定义）对象
+      */
+    val taskDAO = DAOFactory.getTaskDAO()
+    val task = taskDAO.findTaskById(taskId)
+    val taskParamsJsonObject = JSON.parseObject(task.taskParams)
+//    monitorState(sqlContext,sc,taskParamsJsonObject,taskId)
+//    topnMonitorcarcount(sqlContext,sc,rdd1,taskParamsJsonObject,taskId)
+  }
+  def monitorState(sqlContext:SQLContext,sc:SparkContext,taskParamsJsonObject:JSONObject,taskId:Int): Unit ={
+    /**
+      * 给定一个时间段，统计出卡口数量的正常数量，异常数量，还有通道数
+      * 异常数：每一个卡口都会有n个摄像头对应每一个车道，
+      * 如果这一段时间内卡口的信息没有第N车道的信息的话就说明这个卡口存在异常。
+      * 这需要拿到一份数据（每一个卡口对应的摄像头的编号）
+      * 模拟数据在monitor_camera_info临时表中
+      */
+    /**
+      * task.getTaskParams()是一个json格式的字符串   封装到taskParamsJsonObject
+      * 将 task_parm字符串转换成json格式数据。
+      */
+    val startDate = ParamUtils.getParam(taskParamsJsonObject, Constants.PARAM_START_DATE)
+    val endDate = ParamUtils.getParam(taskParamsJsonObject, Constants.PARAM_END_DATE)
+    val sql = "SELECT * FROM monitor_flow_action where date>='" + startDate + "' " + "AND date<='" + endDate + "'"
+    val sql2 = "SELECT * FROM monitor_camera_info "
+    val rdd = sqlContext.sql(sql2).rdd.map(x => (x.get(0), x.get(1))).groupByKey()
+    val rdd2 = sqlContext.sql(sql).rdd.map(x => (x.get(1), x.get(2))).groupByKey()
+    val noraml_monitor_count = sc.longAccumulator
+    val normal_camera_count = sc.longAccumulator
+    val abnormal_monitor_count = sc.longAccumulator
+    val abnormal_camera_count = sc.longAccumulator
+    val acmi = new MonitorAndCameraStateAccumulator
+    sc.register(acmi,"myAcc")
+    rdd2.leftOuterJoin(rdd).map(x => {
+      val list1 = x._2._1.toList
+      val buffer = ListBuffer[String]()
+      var flag = true
+      var count = 0
+      for (elem <- x._2._2.get.toList) {
+        if (!list1.contains(elem)) {
+          buffer.append(elem.toString)
+          abnormal_camera_count.add(1)
+          count += 1
+          flag = false
+        } else {
+          normal_camera_count.add(1)
+        }
+      }
+      if (flag != false) {
+        noraml_monitor_count.add(1)
+      } else {
+        abnormal_monitor_count.add(1)
+      }
+      (x._1, buffer.mkString(",")+"|count:"+count)
+    }).foreach(x=>{
+      acmi.add(x._1+":"+x._2)
+    })
+    saveMonitorState(taskId,noraml_monitor_count.value,normal_camera_count.value,abnormal_monitor_count.value
+      ,abnormal_camera_count.value,acmi.value.substring(0,acmi.value.length-1))
+  }
+  def saveMonitorState(taskId:Long,nmc:Long,ncc:Long,amc:Long,acc:Long,acmi:String): Unit ={
+    /**
+      * 将卡扣状态信息存入数据库
+      */
+    val monitorState = new MonitorState(taskId,nmc.toString,ncc.toString,amc.toString,acc.toString,acmi)
+    val monitorDAO = DAOFactory.getMonitorDAO()
+    monitorDAO.insertMonitorState(monitorState)
+  }
+  def topnMonitorcarcount(sqlContext:SQLContext,sc:SparkContext,rdd1:RDD[Row],taskParamsJsonObject:JSONObject,taskId:Int): Unit ={
+    /**
+      * 通过Constants.FIELD_TOP_NUM从数据库JSON对象中获取topnum
+      */
+    val topnnum = ParamUtils.getParam(taskParamsJsonObject, Constants.FIELD_TOP_NUM).toInt
+    val topn_monitorcarcount = rdd1.map(x=> (x.get(1),x)).groupByKey().map(x=>(x._2.size,x._1.toString)).top(topnnum).map(x=>(x._2,x._1))
+    /**
+      * 获取topn
+      */
+    val array =ArrayBuffer[TopNMonitor2CarCount]()
+    topn_monitorcarcount.foreach(x=>{
+      val topNMonitor2CarCount = new TopNMonitor2CarCount(taskId,x._1,x._2.toString)
+      array.append(topNMonitor2CarCount)
+    })
+    saveTopnMonitorcarcount(array)
+  }
+  def saveTopnMonitorcarcount(topn_monitorcarcount:ArrayBuffer[TopNMonitor2CarCount]): Unit ={
+    /**
+      * 将topn传入数据库
+      */
+    val monitorDAO = DAOFactory.getMonitorDAO()
+    monitorDAO.insertBatchTopN(topn_monitorcarcount.toList)
+  }
+}
