5e6b34ca90583d5390eb1386e55cfb92333aa21a
as897379351 <44804663+as897379351@users.noreply.github.com>
2019-01-16 19:53:20 +0800

    自定义累加器更新

--- src/main/scala/cn/qphone/spark/skynet/MonitorAndCameraStateAccumulator.scala
+++ src/main/scala/cn/qphone/spark/skynet/MonitorAndCameraStateAccumulator.scala
@@ -1,10 +1,31 @@
 package cn.qphone.spark.skynet
 
-/**
-  * @Auther: zhuguangyuan 1159814737@qq.com
-  * @Date: 2019/1/14 22:24
-  * @Description: ${Description}
-  */
-object MonitorAndCameraStateAccumulator {
+import org.apache.spark.util.AccumulatorV2
 
+/**
+  * @Auther: xjw
+  * @Date: 2019/1/14 22:24
+  * @Description:自定义累加器
+  */
+class MonitorAndCameraStateAccumulator extends AccumulatorV2[String, String]{
+  private var res = ""
+  override def isZero: Boolean = {res == ""}
+
+  override def copy(): AccumulatorV2[String, String] = {
+    val newMyAcc = new MonitorAndCameraStateAccumulator
+    newMyAcc.res = this.res
+    newMyAcc
+  }
+
+  override def reset(): Unit = res = ""
+
+  override def add(v: String): Unit = res += v +"-"
+
+  override def merge(other: AccumulatorV2[String, String]): Unit = other match {
+    case o : MonitorAndCameraStateAccumulator => res += o.res
+    case _ => throw new UnsupportedOperationException(
+      s"Cannot merge ${this.getClass.getName} with ${other.getClass.getName}")
+  }
+
+  override def value: String = res
 }
